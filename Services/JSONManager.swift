a78d995ed888573f847032c55b9bf86dd0737f20
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-01-15 19:03:04 +0300

    Delete extension for Bundle

--- Services/JSONManager.swift
+++ Services/JSONManager.swift
@@ -11,26 +11,6 @@ final class JSONManager {
     
 }
 
-extension Bundle {
-    func decode<T: Decodable>(with file: String) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Could not find \(file) in the project")
-        }
-        
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Could not load \(file) in the project")
-        }
-        
-        let decoder = JSONDecoder()
-        
-        guard let loadedData = try? decoder.decode(T.self, from: data) else {
-            fatalError("Could not decode \(file) in the project")
-        }
-        
-        return loadedData
-    }
-}
-
 extension Groups {
     func getJSON(host: URLOptions,
                  scheme: URLOptions,
